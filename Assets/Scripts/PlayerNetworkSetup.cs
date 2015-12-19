7d6a14ac0805ca3a19551387a77a6b9c2e69e16b
Luko van der Maas <lukovandermaas@gmail.com>
2015-12-19 13:10:17 +0100

    completely changed the objective of the project and started on basic networking

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+	
+	void Start () {
+		if(isLocalPlayer){
+                        GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+			GetComponentInChildren<Camera>().enabled = true;
+			GetComponentInChildren<AudioListener>().enabled = true;
+			GetComponent<CharacterController>().enabled = true;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerSyncRotation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerSyncRotation : NetworkBehaviour {
+
+	[SyncVar] Quaternion syncPlayerRotation;
+	[SyncVar] Quaternion syncCamRotation;
+
+	[SerializeField] Transform playerTransform;
+	[SerializeField] Transform camTransform;
+	[SerializeField] float lerpRate;
+
+	Quaternion lastPlayerRot;
+	Quaternion lastCamRot;
+	[SerializeField] float threshold = 15;
+
+	void Update () {
+		TransmitRotations();
+		LerpRotations();
+	}
+
+	private void LerpRotations () {
+		if (!isLocalPlayer){
+			playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, syncPlayerRotation, lerpRate * Time.deltaTime);
+			camTransform.rotation = Quaternion.Lerp(camTransform.rotation, syncCamRotation, lerpRate * Time.deltaTime);
+		}
+	}
+
+	[Command]
+	void CmdProvideRotationServer(Quaternion playerRot, Quaternion camRot) {
+		syncCamRotation = camRot;
+		syncPlayerRotation = playerRot;
+	}
+
+	[ClientCallback]
+	void TransmitRotations () {
+		if(isLocalPlayer && (Quaternion.Angle(playerTransform.rotation, lastPlayerRot) > threshold || Quaternion.Angle(camTransform.rotation, lastCamRot) > threshold)) {
+			CmdProvideRotationServer(playerTransform.rotation, camTransform.rotation);
+		}
+	}
+}
